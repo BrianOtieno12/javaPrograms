e6aa8cf2979a50101d8340e38ecc488b6b0aa931
BrianOtieno12 <noriamy52@gmail.com>
2022-03-08 05:49:44 +0300

    First Commit

---
+++ Welcome3.java
@@ -0,0 +1,20 @@
+//Printing multiple lines of text with a single statement
+public class Welcome3 {
+    // main method begins execution of java application
+    public static void main(String[] args) {
+        System.out.println("Welcome\nto\nJava\nProgramming");
+    }
+}
+
+/**
+ * \n Newline. Position the screen cursor at the beginning of the next line.
+ * \t Horizontal tab. Move the screen cursor to the next tab stop.
+ * \r Carriage return. Position the screen cursor at the beginning of the
+ * current
+ * line—do not advance to the next line. Any characters output after the
+ * carriage return overwrite the characters previously output on that line.
+ * \\ Backslash. Used to print a backslash character.
+ * \" Double quote. Used to print a double-quote character. For example,
+ * System.out.println("\"in quotes\"");
+ * displays "in quotes"
+ */
